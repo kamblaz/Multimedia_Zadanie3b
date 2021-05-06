f4f67e64bc405a40922be41ce7d58daf3b54cf4f
Kamil Błażejczuk <ita-12@sr.dmcs.pl>
2021-05-06 17:57:38 +0200

    greyscale added

---
+++ filters.js
@@ -0,0 +1,8 @@
+export function none() {}
+
+export function grayscale({ data: d }) {
+    for (let i = 0; i < d.length; i += 4) {
+        const [r, g, b] = [d[i], d[i + 1], d[i + 2]];
+        d[i] = d[i + 1] = d[i + 2] = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    }
+};
