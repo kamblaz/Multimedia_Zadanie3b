6cec9b5da602fbfe5caf4a35c22416dd7b3e86ce
Kamil Błażejczuk <ita-12@sr.dmcs.pl>
2021-05-10 13:20:06 +0200

    progressbars

--- filters.js
+++ filters.js
@@ -1,8 +1,14 @@
-export function none() {}
+var x = 0;
+
+export function none() { x = 0 }
 
 export function grayscale({ data: d }) {
-    for (let i = 0; i < d.length; i += 4) {
+    let y = d.length * x;
+    console.log(x);
+    console.log(y);
+    for (let i = 0; i < d.length; i += 4 + x) {
         const [r, g, b] = [d[i], d[i + 1], d[i + 2]];
         d[i] = d[i + 1] = d[i + 2] = 0.2126 * r + 0.7152 * g + 0.0722 * b;
     }
+    x++;
 };
